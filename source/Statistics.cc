8c234ccd6ed96931c931c49afa790f7bae9aac84
Miguel Rodriguez <miguelrodriguez@ufl.edu>
2015-04-04 21:47:57 -0400

    more
    more

--- source/Statistics.cc
+++ source/Statistics.cc
@@ -58,11 +58,17 @@ void Statistics::Read(char *fromWhere){
 	string line;
 	stat_file.open(fromWhere);
 	char *relName;
+	if(stat_file.is_open()){
+		cout<<"Open";
+	}
 	while(getline(stat_file, line)){
+		cout<<line<<endl;
 		if(!line.substr(0,1).compare("-")!= 0){
 			int idx = line.find(" ");
 			relName = new char[line.length()+1];
 			strcpy(relName, line.substr(1,idx-1).c_str());
+			cout<<relName<<endl;
+			cout<<line.substr(idx+1, line.length()-idx-1).c_str()<<endl;
 			int numTuples = atoi(line.substr(idx+1, line.length()-idx-1).c_str());
 
 			RelStat* tmp = new RelStat;
@@ -73,6 +79,8 @@ void Statistics::Read(char *fromWhere){
 			int idx = line.find(" ");
 			char *attName = new char [line.length()+1];
 			strcpy(attName, line.substr(0,idx).c_str());
+			cout<<attName<<endl;
+			cout<<line.substr(idx+1, line.length()-idx-1).c_str()<<endl;
 			int numDistincts = atoi(line.substr(idx+1, line.length()-idx-1).c_str());
 			relInfo[relName]->attInfo[attName] = numDistincts;
 		}
