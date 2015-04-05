abdf413a77ebeff89135d5a59c7a0542d7a7dcc8
Miguel Rodriguez <miguelrodriguez@ufl.edu>
2015-04-04 21:58:34 -0400

    Read and Write finished
    Read and write finished

--- source/Statistics.cc
+++ source/Statistics.cc
@@ -62,15 +62,12 @@ void Statistics::Read(char *fromWhere){
 		cout<<"Open";
 	}
 	while(getline(stat_file, line)){
-		cout<<line<<endl;
-		if(!line.substr(0,1).compare("-")!= 0){
+
+		if(line.substr(0,1).compare("-")!= 0){
 			int idx = line.find(" ");
 			relName = new char[line.length()+1];
 			strcpy(relName, line.substr(1,idx-1).c_str());
-			cout<<relName<<endl;
-			cout<<line.substr(idx+1, line.length()-idx-1).c_str()<<endl;
 			int numTuples = atoi(line.substr(idx+1, line.length()-idx-1).c_str());
-
 			RelStat* tmp = new RelStat;
 			tmp->numTuples = numTuples;
 			relInfo[relName] = tmp;
@@ -79,8 +76,6 @@ void Statistics::Read(char *fromWhere){
 			int idx = line.find(" ");
 			char *attName = new char [line.length()+1];
 			strcpy(attName, line.substr(0,idx).c_str());
-			cout<<attName<<endl;
-			cout<<line.substr(idx+1, line.length()-idx-1).c_str()<<endl;
 			int numDistincts = atoi(line.substr(idx+1, line.length()-idx-1).c_str());
 			relInfo[relName]->attInfo[attName] = numDistincts;
 		}
