cea83f6c6c4feec363269dfbdd9c3622bfd1eb84
Miguel E. Rodríguez <mer@lin114-02.cise.ufl.edu>
2015-04-18 16:26:28 -0400

    hash table modified

--- source/y.tab.h
+++ source/y.tab.h
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,27 +30,36 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_YY_SOURCE_Y_TAB_H_INCLUDED
+# define YY_YY_SOURCE_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     Name = 258,
-     Float = 259,
-     Int = 260,
-     String = 261,
-     SELECT = 262,
-     GROUP = 263,
-     DISTINCT = 264,
-     BY = 265,
-     FROM = 266,
-     WHERE = 267,
-     SUM = 268,
-     AS = 269,
-     AND = 270,
-     OR = 271
-   };
+  enum yytokentype
+  {
+    Name = 258,
+    Float = 259,
+    Int = 260,
+    String = 261,
+    SELECT = 262,
+    GROUP = 263,
+    DISTINCT = 264,
+    BY = 265,
+    FROM = 266,
+    WHERE = 267,
+    SUM = 268,
+    AS = 269,
+    AND = 270,
+    OR = 271
+  };
 #endif
 /* Tokens.  */
 #define Name 258
@@ -71,13 +77,13 @@
 #define AND 270
 #define OR 271
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 26 "./source/Parser.y"
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
+#line 26 "./source/Parser.y" /* yacc.c:1909  */
+
  	struct FuncOperand *myOperand;
 	struct FuncOperator *myOperator; 
 	struct TableList *myTables;
@@ -88,14 +94,16 @@ typedef union YYSTYPE
 	struct NameList *myNames;
 	char *actualChars;
 	char whichOne;
-}
-/* Line 1529 of yacc.c.  */
-#line 94 "./source/y.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 99 "./source/y.tab.h" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
+int yyparse (void);
+
+#endif /* !YY_YY_SOURCE_Y_TAB_H_INCLUDED  */
